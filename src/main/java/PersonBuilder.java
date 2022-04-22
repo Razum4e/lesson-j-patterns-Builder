65445b139507c385818b49e964f5dbf8066aff27
Aleksandr Razumov <Razum.ya@ya.ru>
2022-04-22 20:00:48 +0200

    init commit

--- src/main/java/PersonBuilder.java
+++ src/main/java/PersonBuilder.java
@@ -6,12 +6,18 @@ public class PersonBuilder {
 
     public Person build() {
         StringBuilder error = new StringBuilder();
-        if (age < 0 || age > 200) error.append(" Возраст");
-        if (name == null) error.append(" Имя");
-        if (surname == null) error.append(" Фамилия");
-        if (error.length() != 0) throw new IllegalStateException("Не заданы или не верны:" + error);
-        if (age != 0) return new Person(name, surname, age, address);
-        else return new Person(name, surname, address);
+        if (age < 0 || age > 200)
+            error.append(" Возраст");
+        if (name == null)
+            error.append(" Имя");
+        if (surname == null)
+            error.append(" Фамилия");
+        if (error.length() != 0)
+            throw new IllegalStateException("Не заданы или не верны:" + error);
+        if (age != 0)
+            return new Person(name, surname, age, address);
+        else
+            return new Person(name, surname, address);
     }
 
     public PersonBuilder setName(String name) {
